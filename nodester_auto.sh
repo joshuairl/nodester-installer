a2c7e0dff3fe18d7dc9054b9593a67307c1b8c24
Joshua Rountree <joshua@swodev.com>
2012-12-06 11:40:03 -0500

    Update nodester_auto.sh

--- nodester_auto.sh
+++ nodester_auto.sh
@@ -167,7 +167,7 @@ case $whoami in
     echo ${NOCOLR}
     sleep 1;
     cd ~/build
-    git clone https://github.com/alejandromg/n.git
+    git clone https://github.com/alejandro/n.git
     cd n && make install
 
     echo ${BLDCYA};
